5112db388db74aaaf4bcbb7c2be5385e2cd663d8
赵学通 <zxt_zel_npl@163.com>
2024-04-04 22:16:15 +0800

    feat: add数据

--- paint.js
+++ paint.js
@@ -1,3 +1,5 @@
+ 
+
 const paint = async () => {
     const scene = new L7.Scene({
       id: 'map',
@@ -16,8 +18,18 @@ const paint = async () => {
         return {
           address: row.address,
           Longitude: ges[0],
+          Latitude: ges[1]
+        }
+      }) 
+
+      const aggregations = window.aggregations.filter(row => row.location).map(row => {
+        const ges = row.location.split(',')
+        return {
+          address: row.address,
+          Longitude: ges[0],
           Latitude: ges[1],
-          text: row.community
+          community: row.community,
+          count: row.count
         }
       }) 
       const pointLayer = new L7.PointLayer({})
@@ -39,30 +51,71 @@ const paint = async () => {
   
         scene.addLayer(pointLayer);
 
-        const text = new L7.PointLayer({})
-        .source(data, {
+        const dataSource = new L7.Source(aggregations, {
           parser: {
             type: 'json',
             x: 'Longitude',
             y: 'Latitude',
+            count: 'count',
+            community: 'community'
           }
-        })
-        .shape('text', 'text')
+        });
+        const textBg = new L7.PointLayer({})
+          .source(dataSource)
+          .shape('circle')
+          .scale('count', {
+            type: 'quantile'
+          })
+          .size('count', [ 5, 10, 15, 20, 25 ])
+          .color('#ffffff')
+          .style({
+            strokeWidth: 1,
+            stroke: 'rgb(73,167,86)',
+            opacity: 0.5
+          });
+        scene.addLayer(textBg);
+
+
+        const text = new L7.PointLayer({})
+        .source(dataSource)
+        .shape('community', 'text')
         .size(10)
-        .color('#ffffff')
+        .color('rgb(73,167,86)')
         .style({
+            color: 'rgb(73,167,86)',
             textAnchor: 'center', // 文本相对锚点的位置 center|left|right|top|bottom|top-left
-            textOffset: [ 0, 0 ], // 文本相对锚点的偏移量 [水平, 垂直]
+            textOffset: [ 0, 10 ], // 文本相对锚点的偏移量 [水平, 垂直]
             spacing: 2, // 字符间距
-            padding: [ 1, 1 ], // 文本包围盒 padding [水平，垂直]，影响碰撞检测结果，避免相邻文本靠的太近
-            stroke: '#000000', // 描边颜色
-            strokeWidth: 0.3, // 描边宽度
-            strokeOpacity: 1.0,
+            // padding: [ 1, 1 ], // 文本包围盒 padding [水平，垂直]，影响碰撞检测结果，避免相邻文本靠的太近
+            // stroke: 'rgb(73,167,86)', // 描边颜色
+            // strokeWidth: 0.3, // 描边宽度
+            // strokeOpacity: 1.0,
             fontFamily: 'Times New Roman',
             textAllowOverlap: true
           });
 
       scene.addLayer(text);
+
+
+        const text1 = new L7.PointLayer({})
+        .source(dataSource)
+        .shape('count', 'text')
+        .size(10)
+        .color('rgb(73,167,86)')
+        .style({
+            color: 'rgb(73,167,86)',
+            textAnchor: 'center', // 文本相对锚点的位置 center|left|right|top|bottom|top-left
+            textOffset: [ 0, -10 ], // 文本相对锚点的偏移量 [水平, 垂直]
+            spacing: 2, // 字符间距
+            // padding: [ 1, 1 ], // 文本包围盒 padding [水平，垂直]，影响碰撞检测结果，避免相邻文本靠的太近
+            // stroke: 'rgb(73,167,86)', // 描边颜色
+            // strokeWidth: 1, // 描边宽度
+            // strokeOpacity: 1.0,
+            fontFamily: 'Times New Roman',
+            textAllowOverlap: true
+          });
+
+      scene.addLayer(text1);
     })
   }
 
