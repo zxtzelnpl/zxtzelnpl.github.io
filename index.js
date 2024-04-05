8d7f259744f74804da614df029b25b2444a2a60c
赵学通 <zxt_zel_npl@163.com>
2024-04-05 22:45:17 +0800

    feat: 页面调整

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 import getDatas from "./getDatas.js";
 import getAggregations from "./getAggregations.js";
 import paint from "./paint.js";
-import aggregation from "./aggregation.js";
-import queryLocation from "./queryLocation.js";
-import show from "./show.js";
-import save from "./save.js";
-import getName from "./getName.js";
-import uniqName from "./uniqName.js";
-import samePoint from "./same-point.js";
+// import aggregation from "./aggregation.js";
+// import queryLocation from "./queryLocation.js";
+// import show from "./show.js";
+// import save from "./save.js";
+// import getName from "./getName.js";
+// import uniqName from "./uniqName.js";
+// import samePoint from "./same-point.js";
 
-getDatas();
-getAggregations();
+await getDatas();
+await getAggregations();
+await paint();
+// function addButton(text, fun) {
+//   const btn = document.createElement("button");
+//   btn.innerHTML = text;
+//   btn.onclick = fun;
+//   document.body.append(btn);
+// }
 
-function addButton(text, fun) {
-  const btn = document.createElement("button");
-  btn.innerHTML = text;
-  btn.onclick = fun;
-  document.body.append(btn);
-}
-
-addButton("请求", queryLocation);
-addButton("下载", save);
-addButton("聚合", aggregation);
-addButton("展现", show);
-addButton("绘图", paint);
-addButton("获取名字", getName);
-addButton("去重名字", uniqName);
-addButton("去重点", samePoint);
+// addButton("请求", queryLocation);
+// addButton("下载", save);
+// addButton("聚合", aggregation);
+// addButton("展现", show);
+// addButton("绘图", paint);
+// addButton("获取名字", getName);
+// addButton("去重名字", uniqName);
+// addButton("去重点", samePoint);
